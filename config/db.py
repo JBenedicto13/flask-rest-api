ca51ef09cfe1b601d20aa9661501ecd30c850581
JB Benedicto <jbenedicto20.jbb@gmail.com>
2024-09-17 19:51:48 +0800

    Clean imports of db.py

--- config/db.py
+++ config/db.py
@@ -1,11 +1,7 @@
 import os
-import os
 from supabase import create_client, Client
 from dotenv import load_dotenv
 load_dotenv()
-
-import os
-from supabase import create_client, Client
 
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
