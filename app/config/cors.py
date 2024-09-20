2426a9a1745d9fee68b657c311a68b3996484002
JB Benedicto <jbenedicto20.jbb@gmail.com>
2024-09-20 19:56:44 +0800

    Change origins in cors

--- app/config/cors.py
+++ app/config/cors.py
@@ -3,4 +3,4 @@ from flask_cors import CORS
 
 def init_cors(app):
     """Initialize CORS configuration for the app."""
-    CORS(app, resources={r"/*": {"origins": ["http://localhost:5000", "http://localhost:5000"]}})  # Replace "*" with specific origins if needed
+    CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "http://localhost:5173"]}})  # Replace "*" with specific origins if needed
